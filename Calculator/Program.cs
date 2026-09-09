41626b212e8610195262a2ba6fb4691ad20c3108
Krinkovic <186858802+Krinkovic@users.noreply.github.com>
2026-09-09 12:09:47 +0200

    Initial commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Calculator
+{
+    internal class Calculator
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Enter an RPN expression <return> (empty string = exit):");
+            }
+        }
+    }
+}
