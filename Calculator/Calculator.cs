741baf35b4dc32cf256000df42e3b9db963cb144
Krinkovic <186858802+Krinkovic@users.noreply.github.com>
2026-09-18 18:32:35 +0200

    Structured the project according to the "4 Architecure" section in the instructions.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using Calculator.Controller;
+
+namespace Calculator
+{
+    internal class Calculator
+    {
+        static void Main(string[] args)
+        {
+            CalculatorController ctr = new CalculatorController();
+            ctr.Run(args);
+        }
+    }
+}
